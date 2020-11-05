15916fa2bf8255963b5fa86f098951b381519c43
user <user@DESKTOP-63NCF81>
2020-11-05 12:55:07 +0200

    Andra

--- src/pkg/Friendly.java
+++ src/pkg/Friendly.java
@@ -9,9 +9,21 @@ public class Friendly extends Character {
 	
 	@ Override
 	void die () {
-		System.out.println("I feel bed");
+		System.out.println("I feel bad");
 		
 	}
+	
+	void healPlayer() {
+		Player.getInstance().getHeal(this.getDamage());
+	
+	}
+
+	void attackEnemy(Enemy enemy) {
+		enemy.takeDamage(this.getDamage());
+	}
+	
+	
+	
 	
 	public Friendly(String name, int level,float health,float damage,float armor ) {
 	super(name,level,health,damage,armor); 
